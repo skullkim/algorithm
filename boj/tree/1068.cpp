66ac20a28379774af9d66bf8d763f503f6882051
skullKim <space1878@naver.com>
2020-08-21 10:12:33 +0000

    boj/tree/1068.cpp

---
+++ boj/tree/1068.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAX_NODES = 50;
+int nodes, deleted_node, root, ans = 0;
+vector<vector<int>> tree(MAX_NODES);
+
+void countLeafNode(int curr_node){
+	if(curr_node >= nodes) return;
+	if(tree[curr_node].empty()){
+		ans++;
+		return;
+	}
+	for(int n = 0; n < tree[curr_node].size(); n++){
+		int next = tree[curr_node][n];
+		if(next == deleted_node) continue;
+		countLeafNode(next);
+	}
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	const int ROOT = -1;
+	cin >> nodes;
+	int parent;
+	for(int n = 0; n < nodes; n++){
+		cin >> parent;
+		if(parent == ROOT) root = n;
+		else tree[parent].push_back(n);
+	}
+	cin >> deleted_node;
+	countLeafNode(root);
+	cout << ans;
+}
