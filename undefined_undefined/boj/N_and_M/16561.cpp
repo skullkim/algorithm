844f94027efedfc77b70789ca31d1a7ada5ea0ba
skullKim <space1878@naver.com>
2020-09-27 13:47:32 +0000

    modified 16561

--- undefined_undefined/boj/N_and_M/16561.cpp
+++ undefined_undefined/boj/N_and_M/16561.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
 const int THREE = 3;
 const int MAX_NUM = 1000;
 int target, cases = 0;
 
+
 void findAllCases(int curr_num, int used){
-	if(curr_num == target && used == THREE){
-		cases++;
+	if(used == THREE - 1){
+		if((target - curr_num) > 0 && (target - curr_num) % THREE == 0){
+			cases++;
+		}
 		return;
 	}
-	if(used > THREE) return;
 	for(int i = 1; i <= MAX_NUM; i++){
 		if(curr_num + i * THREE > target) return;
 		findAllCases(curr_num + i * THREE, used + 1);
