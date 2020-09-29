70ab4af2dcc44ce24f99ec8174c2cee7785c26a2
skullKim <space1878@naver.com>
2020-09-29 14:11:40 +0000

    math1 2004

---
+++ undefined_undefined/boj/math1/2004.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int countNum(int limit, const int target){
+	int cnt = 0;
+	for(int i = target; i <= limit; i *= target) cnt += (limit / i);
+	return cnt;
+}
+
+int main(void){
+	 ios_base::sync_with_stdio(false);
+	 cin.tie(NULL);
+	 const int FIVE = 5;
+	 const int TWO = 2;
+	 int n, m;
+	 cin >> n >> m;
+	 int five_num = countNum(n, FIVE) - countNum(m, FIVE) - countNum(n - m, FIVE);
+	 int two_num = countNum(n, TWO) - countNum(m, TWO) - countNum(n - m, TWO);
+	 cout << min(five_num, two_num);
+}
