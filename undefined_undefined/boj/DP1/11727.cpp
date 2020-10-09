535a44916f2970acf2c0855809d06d4774678369
skullKim <space1878@naver.com>
2020-10-09 06:45:36 +0000

    DP1 11727.cpp

---
+++ undefined_undefined/boj/DP1/11727.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_LEN = 1e3 + 10;
+const int MOD = 1e4 + 7;
+int cache[MAX_LEN];
+
+int fillTile(int curr_len){
+	if(curr_len <= 1) return 1;
+	int & tmp = cache[curr_len];
+	if(tmp) return tmp;
+	return tmp = (fillTile(curr_len - 1) + fillTile(curr_len - 2) + fillTile(curr_len - 2)) % MOD;
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int len;
+	cin >> len;
+	cout << fillTile(len);
+}
