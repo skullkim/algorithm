fcbdb0848c5ce029ff99a165125522a1c8742357
skullKim <space1878@naver.com>
2020-10-09 14:19:19 +0000

    DP1 11052

--- undefined_undefined/boj/DP1/11052.cpp
+++ undefined_undefined/boj/DP1/11052.cpp
@@ -7,19 +7,15 @@ const int MAX_CARDS = 1e3 + 10;
 int card[MAX_CARDS], cache[MAX_CARDS];
 int cards;
 
-void payMaxCost(int bought, int cost){
-	if(bought > cards) return;
-	cache[bought] = max(cache[bought], cost);
-	for(int i = 1; i <= cards; i++){
-		payMaxCost(bought + i, cost + card[i]);
-	}
-}
-
 int main(void){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cin >> cards;
 	for(int i = 1; i <= cards; i++) cin >> card[i];
-	payMaxCost(0, 0);
+	for(int i = 1; i <= cards; i++){
+		for(int j = 1; j <= i; j++){
+			cache[i] = max(cache[i], cache[i - j] + card[j]);
+		}
+	}
 	cout << cache[cards];
 }
