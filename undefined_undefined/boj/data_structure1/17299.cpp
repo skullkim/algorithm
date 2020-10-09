26848485e5e7fe9fbebceb61cf61e2148354b850
skullKim <space1878@naver.com>
2020-10-09 05:30:16 +0000

    data structure1 BOJ 17299

---
+++ undefined_undefined/boj/data_structure1/17299.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	const int INF = 1e6 + 10;
+	int seq_len;
+	cin >> seq_len;
+	vector<int> seq(seq_len + 1);
+	vector<int> num_freq(seq_len + 1, 0);
+	for(int i = 1; i <= seq_len; i++){
+		cin >> seq[i];
+		num_freq[seq[i]]++;
+	}
+	vector<int> ans(seq_len);
+	stack<int> curr_max;
+	curr_max.push(INF);
+	for(int i = seq_len; i >= 1; i--){
+		while(curr_max.top() != INF && num_freq[curr_max.top()] <= num_freq[seq[i]]) curr_max.pop();
+		if(curr_max.top() >= INF) ans[i] = -1;
+		else ans[i] = curr_max.top();
+		curr_max.push(seq[i]);
+	}
+	for(int i = 1; i <= seq_len; i++) cout << ans[i] << " ";
+}
