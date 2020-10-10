9ccff358f31889938a0e34d74ede59f9f559f9e2
skullKim <space1878@naver.com>
2020-10-10 14:10:44 +0000

    DP1 10844

--- undefined_undefined/boj/DP1/10844.cpp
+++ undefined_undefined/boj/DP1/10844.cpp
@@ -7,16 +7,17 @@ int main(void){
 	cin.tie(NULL);
 	const int MAX_LEN = 105;
 	const int MOD = 1e9;
-	int len, cache[MAX_LEN][11];
+	int len;
+	int cache[MAX_LEN][11];
 	memset(cache, 0, sizeof(cache));
 	cin >> len;
-	for(int i = 0; i <= 9; i++) cache[1][i] = 1;
+	for(int i = 1; i <= 9; i++) cache[1][i] = 1;
 	for(int i = 2; i <= len; i++){
-		for(int k = 1; k <= 9; k++){
-			cache[i][k] = (cache[i - 1][k - 1] + cache[i - 1][k + 1]) % MOD;
+		for(int k = 0; k <= 9; k++){
+ 			cache[i][k] = (cache[i - 1][k - 1] + cache[i - 1][k + 1]) % MOD;
 		}
 	}
-	int ans = 0;
-	for(int i = 1; i <= 9; i++) ans += cache[len][i];
-	cout << ans % MOD;
+	long long ans = 0;
+	for(int i = 0; i <= 9; i++) ans = (ans + cache[len][i]) % MOD;
+	cout << ans;
 }
