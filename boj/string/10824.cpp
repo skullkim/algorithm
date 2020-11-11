7179b3301d11b13988157ba043a9e7d09c46ef00
skullKim <space1878@naver.com>
2020-11-11 12:26:21 +0000

    10824.cpp

---
+++ boj/string/10824.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	string tmp;
+	string ab = "", cd = "";
+	for(int i = 0; i < 2; i++){
+		cin >> tmp;
+		ab += tmp;
+	}
+	for(int i = 0; i < 2; i++){
+		cin >> tmp;
+		cd += tmp;
+	}
+	cout << stoi(ab) + stoi(cd);
+}
