6425c324d06b412003dc6d8ca63b46283efd860e
skullKim <space1878@naver.com>
2020-11-13 10:49:48 +0000

    1373.cpp

--- boj/string/1373.cpp
+++ boj/string/1373.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <stack>
 #include <string>
 using namespace std;
 
@@ -7,22 +8,25 @@ int main(void){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	string bi_num;
-	string oct_num = "";
+	stack<string> oct_num;
 	cin >> bi_num;
 	for(int i = bi_num.size() - 1; i >= 0; i -= 3){
 		int tmp = 0;
-		if(i - 3 > 0){
+		if(i - 2 >= 0){
 			for(int k = 2; k >= 0; k--) tmp += ((bi_num[i - k] - '0') * pow(2, k));
 		}	
 		else{
-			if(i - 3 == -2){
+			if(i - 2 == -1){
 				for(int k = 1; k >= 0; k--) tmp += ((bi_num[i - k] - '0') * pow(2, k));
 			}	
-			else if(i - 3 == -3){
+			else if(i - 2 == -2){
 				tmp += ((bi_num[i] - '0') * pow(2, 0));
 			}
 		}
-		oct_num += to_string(tmp);
+		oct_num.push(to_string(tmp));
 	}
-	for(int i = oct_num.size() - 1; i >= 0; i--) cout << oct_num[i];
+	while(!oct_num.empty()){
+		cout << oct_num.top();
+		oct_num.pop();
+	}
 }
