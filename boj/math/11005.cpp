184d359556deebe6dc87171b1ce0f027d2642e2a
skullKim <space1878@naver.com>
2020-11-16 08:52:53 +0000

    11005.cpp

--- boj/math/11005.cpp
+++ boj/math/11005.cpp
@@ -8,7 +8,7 @@ int main(void){
 	const char NUMER_SYS[] = {
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-		'L', 'N', 'M', 'O', 'P', 'Q', 'R', 'S', 'G', 'U', 'V',
+		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
 		'W', 'X', 'Y', 'Z'
 	};
 	int target, num;
