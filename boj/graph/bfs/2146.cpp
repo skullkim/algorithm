6fe04523ff2c4ab3d604dab0e16a88a3faf711f3
skullKim <space1878@naver.com>
2020-12-04 09:48:41 +0000

    2146.cpp

--- boj/graph/bfs/2146.cpp
+++ boj/graph/bfs/2146.cpp
@@ -19,35 +19,12 @@ bool visited[MAX_LEN][MAX_LEN];
 vector<pos> near_sea;
 vector<vector<pair<int, int>>> graph(MAX_LEN);
 
-//void calTerritoryArea(pos first_pos, const int TERRITORY_NUM){
-//	queue<pos> now_pos;
-//	now_pos.push(first_pos);
-//	while(!now_pos.empty()){
-//		pos now = now_pos.front();
-//		graph[now.y][now.x].second = TERRITORY_NUM;
-//		visited[now.y][now.x] = true;
-//		now_pos.pop();
-//		for(int i = 0; i < 4; i++){
-//			pos next = {now.y + DIREC[i].y, now.x + DIREC[i].x};
-//			if(0 > next.y || next.y >= map_len || 0 > next.x || next.x >= map_len) continue;
-//			if(visited[next.y][next.x]) continue;
-//			if(graph[next.y][next.x].first == SEA){
-//				if(near_sea.empty()) near_sea.push_back(now);
-//				else if(near_sea.back().y != now.y || near_sea.back().x != now.x) near_sea.push_back(now);
-//				continue;
-//			}
-//			now_pos.push(next);
-//		}
-//	}
-//}
-
 void calTerritoryArea(pos curr_pos, const int & TERRITORY_NUM){
 	visited[curr_pos.y][curr_pos.x] = true;
 	graph[curr_pos.y][curr_pos.x].second = TERRITORY_NUM;
 	for(int i = 0; i < 4; i++){
 		pos next = {curr_pos.y + DIREC[i].y, curr_pos.x + DIREC[i].x};
 		if(0 > next.y || next.y >= map_len || 0 > next.x || next.x >= map_len || visited[next.y][next.x]) continue;
-		//graph[next.y][next.x].first == SEA ? near_sea.push_back(next) : calTerritoryArea(next, TERRITORY_NUM);
 		if(graph[next.y][next.x].first == SEA){
 			if(!near_sea.empty() && near_sea.back().y == curr_pos.y && near_sea.back().x == curr_pos.x) continue;
 			near_sea.push_back(curr_pos);
@@ -73,23 +50,16 @@ int main(void){
 				calTerritoryArea({i, k}, islands);
 				islands++;
 			}
-//			if(!visited[i][k] && graph[i][k].first == LAND && graph[i][k].second == 0){
-//				calTerritoryArea({i, k}, islands);
-//				islands++;
-//			}
 		}
 	}
-//	for(int i = 0; i < near_sea.size(); i++){
-//			cout << near_sea[i].y << " " << near_sea[i].x << endl;
-//	}
-	int ans = MAX_LEN;
+	const int INF = 1000;
+	int ans = INF;
 	for(int i = 0; i < near_sea.size(); i++){
 		for(int j = 0; j < near_sea.size(); j++){
 			pos land1 = near_sea[i], land2 = near_sea[j];
 			if(j == i || (land1.y == land2.y && land1.x == land2.x)) continue;
 			if(graph[land1.y][land1.x].second == graph[land2.y][land2.x].second) continue;
 			ans = min(ans, abs(land1.y - land2.y) + abs(land1.x - land2.x) - 1);
-			//cout << abs(land1.y - land2.y) + abs(land1.x - land2.y) << endl;
 		}
 	}
 	cout << ans;
