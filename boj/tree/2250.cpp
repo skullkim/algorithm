7f9caa3bf5979e4d6951aa745d4b46c30e2fc2e1
skullKim <space1878@naver.com>
2020-12-09 13:51:41 +0000

    2250.cpp

--- boj/tree/2250.cpp
+++ boj/tree/2250.cpp
@@ -16,17 +16,17 @@ struct Gap{
 };
 Tree tree[MAX];
 Gap gap[MAX];
-int nodes, max_level = -1;
+int nodes, max_level = -1, pos = 1;
 
-int inorderSearch(int curr_node, int level, int curr_col){
-	if(curr_node <= 0) return curr_col;
-	int left = inorderSearch(tree[curr_node].left, level + 1, curr_col);
-	int pos = left + 1;
+void inorderSearch(int curr_node, int level){
+	if(curr_node <= 0) return;
+	inorderSearch(tree[curr_node].left, level + 1);
+	pos++;
 	gap[level].l_gap = min(gap[level].l_gap, pos);
 	gap[level].r_gap = max(gap[level].r_gap, pos);
 	max_level = max(max_level, level);
-	int right = inorderSearch(tree[curr_node].right, level + 1, pos);
-	return right;
+	inorderSearch(tree[curr_node].right, level + 1);
+	return;
 }
 
 int main(void){
@@ -49,7 +49,7 @@ int main(void){
 			break;
 		}
 	}
-	inorderSearch(root, 1, 0);
+	inorderSearch(root, 1);
 
 	int max_with = 0, level = 0;
 	for(int i = 1; i <= max_level; i++){
