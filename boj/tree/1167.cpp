0717e6c29fce84f7a6aa1b4a6738436cb2dd423c
skullKim <space1878@naver.com>
2020-12-16 14:09:36 +0000

    1167.cpp

--- boj/tree/1167.cpp
+++ boj/tree/1167.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstring>
 #include <map>
 using namespace std;
 
@@ -10,19 +11,18 @@ struct Tree{
 	int cost;
 };
 vector<vector<Tree>> tree(MAX);
-int ans = -1, nodes, root = -1;
-map<pair<int, int>, bool> visited;
-//bool visited[MAX][MAX];
+int ans = -1, nodes, end_node;
+bool visited[MAX];
 
 void calMaxCost(int curr_node, int curr_cost){
-	ans = max(ans, curr_cost);
+	visited[curr_node] = true;
+	if(curr_cost > ans){
+		ans = curr_cost;
+		end_node = curr_node;
+	}
 	for(int i = 0; i < tree[curr_node].size(); i++){
 		Tree next = tree[curr_node][i];
-		pair<int, int> v = make_pair(curr_node, next.node);
-		pair<int, int> v2 = make_pair(next.node, curr_node);
-		if(visited.count(v)) continue;
-		visited[v] = true;
-		visited[v2] = true;
+		if(visited[next.node]) continue;
 		calMaxCost(next.node, curr_cost + next.cost);
 	}
 }
@@ -34,7 +34,6 @@ int main(void){
 	int parent, tmp = 0;
 	for(int n = 0; n < nodes; n++){
 		cin >> parent;
-		int idx = 0;
 		while(true){
 			Tree t;
 			cin >> tmp;
@@ -46,6 +45,8 @@ int main(void){
 			tree[parent].push_back(t);
 		}
 	}
-	for(int n = 1; n <= nodes; n++) calMaxCost(n, 0);
+	calMaxCost(1, 0);
+	memset(visited, false, sizeof(visited));
+	calMaxCost(end_node, 0);
 	cout << ans;
 }
