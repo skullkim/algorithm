ec0d373c1d3b6fc4e05f7aeb26825233e3d45811
skullKim <space1878@naver.com>
2021-01-20 13:11:58 +0000

    1463.cpp

--- boj/dp/1463.cpp
+++ boj/dp/1463.cpp
@@ -10,7 +10,7 @@ int main(void){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cin >> n;
-	for(int i = 2; i <= n + 1; i++){
+	for(int i = 2; i <= n; i++){
 		cache[i] = cache[i - 1] + 1;
 		if(i % 3 == 0) cache[i] = min(cache[i / 3] + 1, cache[i]);
 		else if(i % 2 == 0) cache[i] = min(cache[i / 2] + 1, cache[i]);
