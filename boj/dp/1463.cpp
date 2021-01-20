56c5f2ee620c12d7a59f4d8a55fcf9f9eae3588a
skullKim <space1878@naver.com>
2021-01-20 13:05:47 +0000

    boj 1463.cpp

---
+++ boj/dp/1463.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 1e6 + 10;
+int cache[MAX];
+int n;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n;
+	for(int i = 2; i <= n + 1; i++){
+		cache[i] = cache[i - 1] + 1;
+		if(i % 3 == 0) cache[i] = min(cache[i / 3] + 1, cache[i]);
+		else if(i % 2 == 0) cache[i] = min(cache[i / 2] + 1, cache[i]);
+	}
+	cout << cache[n];
+}
