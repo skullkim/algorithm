83e4d6a58d51771749d5ff6a7ca43c9bdb7b5b20
skullKim <space1878@naver.com>
2021-02-25 07:41:19 +0000

    1182_re.cpp

---
+++ boj/brute_force/1182_re.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int n, s, arr[25], an = 0;
+bool b[25];
+
+void f(int sum, int idx, int sz){
+	if(sz >= 1 && sum == s){
+		an++;
+		return;
+	}
+	for(int i = idx; i < n; i++){
+		if(b[i]) continue;
+		b[i] = true;
+		f(sum + arr[i], i + 1, sz + 1);
+		b[i] = false;
+	}
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n >> s;
+	for(int i = 0; i < n; i++) cin >> arr[i];
+	f(0, 0, 0);
+	cout << an;
+}
