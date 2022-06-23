ff85388cb8af6daeaeee43a471288a9ff10b71ef
skullKim <kyk990328@gmail.com>
2022-06-23 12:53:44 +0900

    boj 15649_re

---
+++ java/bruteForce/15649_re/Main.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int n, m;
+    static boolean[] vi = new boolean[9];
+    public static ArrayList<Integer> arr = new ArrayList<>();
+
+    static void f(int currN) {
+        if (arr.size() == m) {
+            for (Integer n : arr) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (vi[i]) continue;
+            arr.add(i);
+            vi[i] = true;
+            f(i);
+            vi[i] = false;
+            arr.remove(arr.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tz = new StringTokenizer(r.readLine());
+        n = Integer.parseInt(tz.nextToken());
+        m = Integer.parseInt(tz.nextToken());
+        f(0);
+    }
+}
