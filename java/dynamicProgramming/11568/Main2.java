6ec133558c4aa90183aa6e15421ee33306862270
skullKim <kyk990328@gmail.com>
2022-07-16 14:27:44 +0900

    11568

--- java/dynamicProgramming/11568/Main2.java
+++ java/dynamicProgramming/11568/Main2.java
@@ -4,12 +4,14 @@ import java.util.*;
 public class Main2 {
 
     static int numbers;
-    static int[] sequence = new int[1_010];
-    static int[] cache = new int[1_1010];
+    static final int MAX = 1_010;
+    static int[] sequence = new int[MAX];
+    static int[] cache = new int[MAX];
     static BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
+        for (int i = 0; i < MAX; i++) cache[i] = 1;
         StringTokenizer st = new StringTokenizer(r.readLine());
         numbers = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(r.readLine());
@@ -19,7 +21,7 @@ public class Main2 {
 
         int ans = 0;
         for (int now = 0; now < numbers; now++) {
-            for (int before = 0; before < numbers; before++) {
+            for (int before = 0; before < now; before++) {
                 if (sequence[before] >= sequence[now]) continue;
                 cache[now] = Math.max(cache[before] + 1, cache[now]);
             }
