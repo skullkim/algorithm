5128187e65d7de668b5b8610fc941c5b9dfe9ae8
skullKim <kyk990328@gmail.com>
2022-07-25 15:06:45 +0900

    9012

---
+++ java/dataStructure/9012/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n;
+    static String b;
+    static BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter w = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        n = Integer.parseInt(st.nextToken());
+        while (n-- > 0) {
+            st = new StringTokenizer(r.readLine());
+            b = st.nextToken();
+            int ans = 0;
+            for (int i = 0; i < b.length(); i++) {
+                if (b.charAt(i) == '(') ans++;
+                else if (b.charAt(i) == ')') ans--;
+                if (ans < 0) break;
+            }
+            if (ans == 0) w.write("YES\n");
+            else w.write("NO\n");
+        }
+        w.flush();
+    }
+}
