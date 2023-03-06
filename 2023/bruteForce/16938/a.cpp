c648d300a8db4013e101c05c94ca02be6b843e9a
skullkim <kyk990328@gmail.com>
2023-03-06 10:41:20 +0900

    16938

---
+++ 2023/bruteForce/16938/a.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, minN, maxN, x, ans = 0;
+vector<int> v;
+
+void f(int idx, vector<int> t, int sum, int maxNN, int minNN) {
+	if (t.size() > n || sum > maxN) return;
+	if (t.size() <= n && minN <= sum && sum <= maxN && (maxNN - minNN) >= x) {
+		ans++;
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		t.push_back(v[i]);
+		f(i + 1, t, sum + v[i], max(maxNN, v[i]), min(minNN, v[i]));
+		t.pop_back();
+	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> minN >> maxN >> x;
+	for (int i = 0, tmp; i < n; i++) {
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	
+	vector<int> t;
+	f(0, t, 0, 0, 987654321);
+	cout << ans;
+}
