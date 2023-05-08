58351ef18da0cade2dda20be0a1842fe5e35573f
skullkim <kyk990328@gmail.com>
2023-05-08 17:24:59 +0900

    3343

--- 2023/bruteForce/3343/a.cpp
+++ 2023/bruteForce/3343/a.cpp
@@ -1,46 +1,54 @@
 #include <iostream>
 #include <algorithm>
+#include <cmath>
+typedef long long ll;
 
 using namespace std;
 
 struct Flower {
-	int flowers;
-	int amount;
+	ll flowers;
+	ll amount;
 };
 
-long long target;
-long long answer = 1e+18 + 2;
-Flower f1, f2;
-
-long long currTarget, currAmount = 0;
-
-void f() {
-	if (currTarget <= 0) {
-		answer = min(currAmount, answer);
-		return;
-	}
-
-	currTarget -= f1.flowers; currAmount += f1.amount;
-	f();
-	currTarget += f1.flowers; currAmount -= f1.amount;
-
-	currTarget -= f2.flowers; currAmount += f2.amount;
-	f();
-	currTarget += f2.flowers; currAmount -= f2.amount;
-}
+ll target;
+ll answer = 1e+18 + 2;
+Flower cheaper, expensive;
 
 int main(void) {
 	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
 	int flowers, amount;
 	cin >> target;
 	cin >> flowers >> amount;
-	f1 = {flowers, amount};
+	cheaper = {flowers, amount};
 	cin >> flowers >> amount;
-	f2 = {flowers, amount};
+	expensive = {flowers, amount};
 
-	currTarget = target;
+//	cout << target << endl;
+//	cout << cheaper.amount << " " << cheaper.flowers << endl;
+//	cout << expensive.amount << " " << expensive.flowers << endl;
 
-	f();
+	if (cheaper.amount*expensive.flowers > cheaper.flowers*expensive.amount) {
+		Flower tmp = cheaper;
+		cheaper = expensive;
+		expensive = tmp;
+	}
+//	cout << endl;
+//	cout << target << endl;
+//	cout << cheaper.flowers << " " << cheaper.amount << endl;
+//	cout << expensive.flowers << " " << expensive.amount << endl << endl;
+	
+	for (int i = 0; i < cheaper.flowers; i++) {
+		ll bought = (ll) ceil((double) (target - i * expensive.flowers) / cheaper.flowers);
+//		cout << bought << " " << i << endl;;
+		bool isOver = false;
+		if (bought < 0) {
+			bought = 0;
+			isOver = true;
+		}
+		answer = min(answer, expensive.amount * i + cheaper.amount * bought);
+		if (isOver) break;
+	}
 	cout << answer;
 }
