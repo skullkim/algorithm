d277ed50af7a627ad65a5c35fd27d9366244d8b8
skullkim <kyk990328@gmail.com>
2023-05-17 18:41:38 +0900

    1456

--- 2023/dynamicProgramming/1456/a.cpp
+++ 2023/dynamicProgramming/1456/a.cpp
@@ -1,48 +1,51 @@
 #include <iostream>
-#include <cstring>
+#include <cstring> 
 #include <cmath>
+typedef long long ll;
 
 using namespace std;
 
-const int MAX_IDX = 1e7;
+const int MAX_IDX = 1e7 + 1;
 const int MAX = MAX_IDX * 2;
 bool isNotPrime[MAX_IDX];
 bool isNotPrime2[MAX_IDX * 2];
+bool isPrime[MAX_IDX];
+bool vi[MAX_IDX];
 
+//bool isPrime(int num) {
+//	if (num < MAX_IDX && isNotPrime[num]) return false;
+//	else if (MAX_IDX <= num && num <= MAX && isNotPrime2[num - MAX_IDX]) return false;
+//	return true;
+//}
+//
 int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	int start, end;
 	cin >> start >> end;
+	
+	memset(isPrime, true, sizeof(isPrime));
 
-	isNotPrime[0] = true; isNotPrime[1] = true;
-	for (int i = 2; i <= MAX ; i++) {
-		if (i < MAX_IDX && isNotPrime[i]) continue;
-		else if (MAX_IDX <= i && i <= MAX && isNotPrime2[i - MAX_IDX]) continue;
-
-		for (int k = i + i; k <= MAX; k += i) {
-			if (k < MAX_IDX && !isNotPrime[k]) {
-				isNotPrime[k] = true;
-			}
-			else if (MAX_IDX <= k && k <= MAX && !isNotPrime2[k - MAX_IDX]) {
-				isNotPrime2[k - MAX_IDX] = true;
-			}
-		}
+//	isNotPrime[0] = true; isNotPrime[1] = true;
+	isPrime[0] = false; isPrime[1] = false;
+	for (ll i = 2; i < MAX_IDX; i++) {
+		if (!isPrime[i]) continue;
+		for (ll k = i + i; k < MAX_IDX; k += i) isPrime[k] = false;
 	}
 
 	int ans = 0;
-	for (int i = start; i <= end; i++) {
-		if (i < MAX_IDX && isNotPrime[i]) continue;
-		else if (MAX_IDX <= i && i <= MAX && isNotPrime2[i - MAX_IDX]) continue;
-		int sqrtVal = (int)sqrt(i);
-		double isNumber = sqrt(i) - sqrtVal;
-		cout << sqrtVal << endl;
-		if (isNumber > 0) continue;
-		if (sqrtVal < MAX_IDX && isNotPrime[sqrtVal]) continue;
-		else if (MAX_IDX <= sqrtVal && sqrtVal <= MAX && isNotPrime2[sqrtVal - MAX_IDX]) continue;
-		cout << i << endl;
-		ans++;
+	for (ll k = 2; k < MAX_IDX; k++) {
+		if (!isPrime[k]) continue;
+		int prime = k * k;
+		if (prime % k != 0) break;
+		while(prime <= end) {
+			if (start <= prime) {
+				ans++;
+			}
+			prime *= k;
+			if (prime % k != 0) break;
+		}
 	}
 	cout << ans;
 }
