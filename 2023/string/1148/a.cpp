8955f867ad27d43bb52b48e66ad5ee4ef27ce8cd
skullkim <kyk990328@gmail.com>
2023-09-03 20:58:15 +0900

    1148

--- 2023/string/1148/a.cpp
+++ 2023/string/1148/a.cpp
@@ -21,6 +21,7 @@ vector<string> filterNonExistentWords(map<char, int> boardChars, vector<string> 
 		map<char, int> chCount;
 		for (int c = 0; c < dictionary[dicIdx].size(); c++) {
 			char ch = dictionary[dicIdx][c];
+			// board에 존재하지 않는 문자면 추가하지 않는다
 			if (!boardChars[ch]) {
 				exist = false;
 				break;
@@ -28,7 +29,9 @@ vector<string> filterNonExistentWords(map<char, int> boardChars, vector<string> 
 			if (!chCount[ch]) chCount[ch] = 1;
 			else chCount[ch]++;
 		}
-
+		
+		if (!exist) continue;
+		// board에 있는 임의의 알파벳 겟수 < 필요한 알파벳 갯수
 		for (auto ch : chCount) {
 			if (boardChars[ch.first] < ch.second) {
 				exist = false;
@@ -65,9 +68,15 @@ int main(void) {
 		map<char, int> boardChars = countBoardChar(board);
 		vector<string> existedWords = filterNonExistentWords(boardChars, dictionary);
 
+		for (int i = 0; i < existedWords.size(); i++) {
+			cout << existedWords[i] << "\n";
+		}
+
 		map<char, int> used;
 		int minUsed = MAX, maxUsed = 0;
 		int uniqueChars = boardChars.size();
+		// board에 있는 문자들을 순회한다.
+		// boardChars의 키값을 고유하므로 중복 체크가 필요 없다
 		for (auto b : boardChars) {
 			char boardChar = b.first;
 			used[boardChar] = 0;
@@ -75,12 +84,15 @@ int main(void) {
 				bool hasChar = false;
 				for (int c = 0; c < existedWords[k].size(); c++) {
 					char ch = existedWords[k][c];
+					// 문자열의 문자가 board에 존재하지 않으면
 					if (boardChar != ch) continue;
+					// 기존에 탐색한 board의 문자 중 사용하지 않은 문자면
 					if (!used[ch]) {
 						used[ch] = 1;
 						uniqueChars--;
 					}
 					else used[ch]++;
+					// board 문자의 최대, 최소 사용 횟수를 갱신한다.
 					minUsed = min(minUsed, used[ch]);
 					maxUsed = max(maxUsed, used[ch]);
 					hasChar = true;
@@ -91,10 +103,11 @@ int main(void) {
 			}
 		}
 		
-		if (uniqueChars) {
+		if (uniqueChars) { // board에 있는 문자 중 사전에 있는 문자열 스펠링에 사용하지 않은 문제가 있으면
 			minUsed = 0;
 		}
 
+		// board문자 중 최소, 최대로 많이 사용만 문자들을 찾는다.
 		string minUsedChars = "", maxUsedChars = "";
 		for (auto u : used) {
 			if (minUsed == u.second) minUsedChars += u.first;
