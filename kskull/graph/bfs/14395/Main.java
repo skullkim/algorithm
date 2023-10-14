3ef591d54d7933d0fb2a883a966cae36bfe8ec90
skullKim <kyk990328@gmail.com>
2023-10-14 17:04:16 +0900

    14395

--- kskull/graph/bfs/14395/Main.java
+++ kskull/graph/bfs/14395/Main.java
@@ -6,8 +6,14 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
+    static Map<Long, Boolean> vi = new HashMap<>();
 
     static class Calculation {
+        private static final int MULTIPLY = 0;
+        private static final int PLUS = 1;
+        private static final int MINUS = 2;
+        private static final int DIVISION = 3;
+
         public long result;
         public String operation;
 
@@ -16,26 +22,35 @@ public class Main {
             this.operation = operation;
         }
 
-        public void removeLastOperation() {
-            if (operation.length() >= 1) {
-                operation = operation.substring(0, operation.length() - 1);
+        public Boolean isVisited(int op) {
+            if (result == 0) return false;
+            switch (op) {
+                case MULTIPLY:
+                    return vi.get(result * result);
+                case PLUS:
+                    return vi.get(result + result);
+                case MINUS:
+                    return vi.get(result - result);
+                case DIVISION:
+                    return vi.get(result / result);
             }
+            return false;
         }
 
         public Calculation caculate(int op) {
             Calculation tmp = new Calculation(this.result, this.operation);
             switch (op) {
-                case 0:
+                case MULTIPLY:
+                    tmp.result *= tmp.result;
+                    tmp.operation += "*";
+                    break;
+                case PLUS:
                     tmp.result += tmp.result;
                     tmp.operation += "+";
                     break;
-                case 1:
+                case MINUS:
                     tmp.result -= tmp.result;
                     tmp.operation += " -";
-                    break;
-                case 2:
-                    tmp.result *= tmp.result;
-                    tmp.operation += "*";
                     break;
                 case 3:
                     if (tmp.result == 0) break;
@@ -66,15 +81,15 @@ public class Main {
             Calculation calculation = q.poll();
 
             if (calculation.result <= 0) break;
-            if (calculation.result == target 
-                    && (answer.result == -1 || answer.operation.compareTo(calculation.operation) > 0)
-                    && (answer.operation.isBlank() || answer.operation.length() >= calculation.operation.length())) {
+            if (calculation.result == target) {
                 answer.operation = calculation.operation;
                 answer.result = target;
             }
 
             for (int i = 0; i < 4; i++) {
+                if (calculation.isVisited(i) != null) continue;
                 Calculation nextCalculation = calculation.caculate(i);
+                vi.put(nextCalculation.result, true);
                 if (nextCalculation.result == calculation.result
                         || nextCalculation.result == 0
                         || nextCalculation.result == givenNumber) continue;
