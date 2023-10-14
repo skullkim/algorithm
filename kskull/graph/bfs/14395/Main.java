ac9fb4158ab1ce922bc1d6edc0a3d9411dc8e7b8
skullKim <kyk990328@gmail.com>
2023-10-14 16:05:22 +0900

    14395

---
+++ kskull/graph/bfs/14395/Main.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    static class Calculation {
+        public long result;
+        public String operation;
+
+        public Calculation(long result, String operation) {
+            this.result = result;
+            this.operation = operation;
+        }
+
+        public void removeLastOperation() {
+            if (operation.length() >= 1) {
+                operation = operation.substring(0, operation.length() - 1);
+            }
+        }
+
+        public Calculation caculate(int op) {
+            Calculation tmp = new Calculation(this.result, this.operation);
+            switch (op) {
+                case 0:
+                    tmp.result += tmp.result;
+                    tmp.operation += "+";
+                    break;
+                case 1:
+                    tmp.result -= tmp.result;
+                    tmp.operation += " -";
+                    break;
+                case 2:
+                    tmp.result *= tmp.result;
+                    tmp.operation += "*";
+                    break;
+                case 3:
+                    if (tmp.result == 0) break;
+                    tmp.result /= tmp.result;
+                    tmp.operation += "/";
+                    break;
+            }
+           return tmp;
+        }
+    };
+
+    static Queue<Calculation> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int givenNumber = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+        if (givenNumber == target) {
+            bw.write("0" + "\n");
+            bw.flush();
+            return;
+        }
+
+        q.add(new Calculation(givenNumber, ""));
+        Calculation answer = new Calculation(-1, "");
+        int answerLength = 0;
+        int a = 0;
+        while (!q.isEmpty()) {
+            Calculation calculation = q.poll();
+
+            //bw.write(calculation.operation + ", " + calculation.result + ", " + calculation.operation.length() + "\n");
+            if (answerLength != 0 && calculation.operation.length() > answerLength) break;
+            if (calculation.result < 0) break;
+            if (calculation.result == target 
+                    && (answer.result == -1 || answer.operation.compareTo(calculation.operation) > 0)) {
+                answer.operation = calculation.operation;
+                answer.result = target;
+                if (answerLength == 0) {
+                    answerLength = calculation.operation.length();
+                }
+            }
+
+            for (int i = 0; i < 4; i++) {
+                Calculation nextCalculation = calculation.caculate(i);
+                if (nextCalculation.result == calculation.result
+                        || nextCalculation.result == 0
+                        || nextCalculation.result == givenNumber) continue;
+                q.add(nextCalculation);
+            }
+            a++;
+        }
+
+        if (answer.result != target) {
+            bw.write("-1\n");
+            bw.flush();
+            return;
+        }
+
+        bw.write(answer.operation + "\n");
+        bw.flush();
+    }
+}
