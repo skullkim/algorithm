b3f5c8cb9d5c5b77d906488e1903d6495252eb1a
skullKim <kyk990328@gmail.com>
2023-10-20 22:35:19 +0900

    17088

--- kskull/bruteForce/17088/Main.java
+++ kskull/bruteForce/17088/Main.java
@@ -8,57 +8,7 @@ public class Main {
     static StringTokenizer st;
 
     static int sequenceLength;
-    static ArrayList<Integer> sequence = new ArrayList<>();
     static int answer = 987654321;
-
-    static void print(ArrayList<Integer> arr) {
-        for (Integer a : arr) {
-            System.out.print(a + " ");
-        }
-        System.out.println();
-    }
-
-    static boolean isAnswer(ArrayList<Integer> diffs) {
-        int d = diffs.get(0);
-        for (Integer diff : diffs) {
-            if (d != diff) return false;
-        }
-        return true;
-    }
-
-    static ArrayList<Integer> changeDiff(int index, int number, ArrayList<Integer> diffs) {
-        if (index == 0) {
-            diffs.set(0, diffs.get(0) + number * (-1));
-        } else if (index == sequenceLength - 1) {
-            diffs.set(index - 1, diffs.get(index - 1) + number);
-        } else {
-            diffs.set(index, diffs.get(index) + number * (-1));
-            diffs.set(index - 1, diffs.get(index - 1) + number);
-        }
-        return diffs;
-    }
-
-    static void findMin(int index, int operations, ArrayList<Integer> diffs) {
-        if (index >= sequenceLength) return;
-        if (isAnswer(diffs)) {
-            answer = Math.min(answer, operations);
-            return;
-        }
-
-        findMin(index + 1, operations, diffs);
-
-        sequence.set(index, sequence.get(index) + 1);
-        diffs = changeDiff(index, 1, diffs);
-        findMin(index + 1, operations + 1, diffs);
-        diffs = changeDiff(index, -1, diffs);
-        sequence.set(index, sequence.get(index) - 1);
-
-        sequence.set(index, sequence.get(index) - 1);
-        diffs = changeDiff(index, -1, diffs);
-        findMin(index + 1, operations + 1, diffs);
-        diffs = changeDiff(index, 1, diffs);
-        sequence.set(index, sequence.get(index) + 11);
-    }
 
     static class Ele {
         public int value;
@@ -157,12 +107,6 @@ public class Main {
                 answer = Math.min(answer, changed);
             }
         }
-
-//        ArrayList<Integer> diffs = new ArrayList<>();
-//        for (int i = 1; i < sequenceLength; i++) {
-//            int diff = sequence.get(i) - sequence.get(i - 1);
-//            diffs.add(diff);
-//        }
 
         if (answer == 987654321) {
             bw.write("-1\n");
