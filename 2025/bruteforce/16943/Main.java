ea29e869886a3bcb918d3a80b781f2e420bc04da
skullkim <kyk990328@gmail.com>
2025-06-07 13:44:46 +0900

    1694

--- 2025/bruteforce/16943/Main.java
+++ 2025/bruteforce/16943/Main.java
@@ -6,20 +6,23 @@ class Main {
   static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
   static char[] a;
   static char[] b;
+  static int bInt;
   static int ans = -1;
 
   static void f(boolean[] vi, String c) {
     if (c.length() > b.length) return;
     if (c.length() == a.length && c.length() == b.length) {
+      int cc = Integer.parseInt(c);
+      if (cc > bInt) return;
       //System.out.println(c);
-      ans = Math.max(ans, Integer.parseInt(c));
+      ans = Math.max(ans, cc);
       return;
     }
     if (c.length() == b.length) return;
 
     for (int i = 0; i < a.length; i++) {
       if (c.length() == 0 && a[i] == '0') continue;
-      if (a[i] > b[c.length()] || vi[i]) continue;
+      if (vi[i]) continue;
       vi[i] = true;
       f(vi, c + a[i]);
       vi[i] = false;
@@ -29,7 +32,9 @@ class Main {
   public static void main(String[] args) throws IOException {
     StringTokenizer st = new StringTokenizer(br.readLine());
     a = st.nextToken().toCharArray();
-    b = st.nextToken().toCharArray();
+    String tmp = st.nextToken();
+    b = tmp.toCharArray();
+    bInt = Integer.parseInt(tmp);
     
     boolean[] vi = new boolean[a.length];
     f(vi, "");
